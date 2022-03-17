ec1e3ebdc83751d8047a1f4a1bbc97629554a3ac
Edwin Carl Flores <Edwin.Flores@sensis.com.au>
2022-03-17 20:54:46 +0800

    Added extraction of auth code from URL params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-redirect';
+  public authCode: string = '';
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.authCode = params['authCode'];
+    });
+  }
 }
